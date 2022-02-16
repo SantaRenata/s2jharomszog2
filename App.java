a2df923421c0f0f6401a4d7b74f8c34cb0797f42
SantaRenata <renatasanta2002@gmail.com>
2022-02-16 09:58:30 +0100

    Kezdes

---
+++ App.java
@@ -0,0 +1,17 @@
+/*
+* File: App.java
+* Author: Sánta Renáta Diána
+* Copyright: 2022, Sánta Renáta Diána
+* Group: Szoft I N
+* Date: 2022-02-16
+* Github: https://github.com/SantaRenata/
+* Licenc: GNU GPL
+*/
+
+class App {
+	
+	public static void main (String[] args) {
+		System.out.println("Santa Renata Diana, 2022-02-16, Szoft I N");
+		System.out.println("Feladat 0306");
+		System.out.println("Haromszog teruletszamitas");
+	}
