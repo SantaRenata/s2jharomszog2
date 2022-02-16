7c2ae629596d08a11a922b47d036c8baf2455c79
SantaRenata <renatasanta2002@gmail.com>
2022-02-16 10:08:56 +0100

    Kesz az elso verzio

--- App.java
+++ App.java
@@ -8,10 +8,25 @@
 * Licenc: GNU GPL
 */
 
+import java.util.Scanner;
 class App {
 	
 	public static void main (String[] args) {
 		System.out.println("Santa Renata Diana, 2022-02-16, Szoft I N");
 		System.out.println("Feladat 0306");
 		System.out.println("Haromszog teruletszamitas");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.print("Alap: ");
+		double alap = sc.nextDouble();
+		
+		System.out.print("Magassag: ");
+		double magassag = sc.nextDouble();
+		
+		double terulet = (alap*magassag)/2;
+		System.out.print("Terulet: " + terulet);
+		
+		
 	}
+}
